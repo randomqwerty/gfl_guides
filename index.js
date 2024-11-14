48350cdb597ffa24bc02b1482cc4f09c71540d1b
randomqwerty <randomqwerty@users.noreply.github.com>
2024-11-13 20:10:09 -0500

    Add assets and URL query parameter logic, adjust guide_export

--- index.js
+++ index.js
@@ -8,17 +8,31 @@
 			el.value = opt;
 			selectGuide.appendChild(el);
 		};
+		
+		// Get query strings from URL
+		const queryString = window.location.search;
+		const urlParams = new URLSearchParams(queryString);
+		var guide = urlParams.get("guide");
+		
+		// Load data if query strings are not blank
+		if (guide != null) {
+			console.log(guide);
+			$('[name="selectGuide"]').val(guide);
+			UpdateData();
+		};	
 	});
 	
 	function UpdateData() {		
 		$.getJSON('./guide_export.json', function(data){
 			// Get selected guide, filter the JSON, extract only 
 			var guide = $('[name="selectGuide"]').val();
+			console.log(guide);
 			var filteredData = data.filter(x => x.title == guide);
 			
-			// Extract subtitle and data from the JSON
+			// Extract subtitle, data, and filenames from the JSON
 			sub_title_orig = filteredData.map(function (x) {return x.sub_title;})
 			data_orig = filteredData.map(function (x) {return x.data;})
+			fname = filteredData.map(function (x) {return x.filename;})
 			
 			// Keep only the first instance of each subtitle
 			sub_title = [];
@@ -26,16 +40,30 @@
 				sub_title.push(index == 0 ? item : (item == sub_title_orig[index-1] ? '' : item));
 			});
 			
-			// Insert subtitles where applicable
-			data = [];
+			// Insert subtitles and filenames into the data where applicable
+			data_final = [];
 			data_orig.forEach((item, index) => {
-				data.push(sub_title[index] == '' ? data_orig[index] : '<h2>' + sub_title[index] + '</h2><br>' + data_orig[index]);
+				var x = data_orig[index] ? data_orig[index] : (fname[index] ? '<img width=50% src="assets/' + fname[index] + '">' : '');
+				
+				x = sub_title[index] == '' ? x : '<h2>' + sub_title[index] + '</h2><br>' + x;
+				x = x.replaceAll('src="/girlsfrontline/sites/girlsfrontline/files/inline-images/', 'src="assets/');
+				data_final.push(x);
 			});
 			
 			// Populate divs
 			$('#guideHeader').text(filteredData[0].title);
 			$('#guideDesc').text(filteredData[0].description);
 			$('#guideAuthors').text('By: ' + filteredData[0].authors);
-			$('#guideData').html(data.join('<br>'));
+			$('#guideData').html(data_final.join('<br>'));
+			
+			// Update URL with new query strings if applicable
+			var newURL;
+			var baseURL = window.location.href.split('?')[0];
+			if (guide.length != 0) {
+				newURL = encodeURI(baseURL + "?guide=" + guide);
+			} else {
+				newURL = baseURL;
+			};
+			window.history.pushState('obj', 'Title', newURL);
 		});
 	};
